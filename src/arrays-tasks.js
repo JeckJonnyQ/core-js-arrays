fc09512c081d472a1629864bb851878d247240aa
Reut Vladislav <reut-2011@mail.ru>
2024-01-04 12:55:33 +0300

    feat: add function getTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -208,8 +208,11 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  if (n >= arr.length) {
+    return arr.slice();
+  }
+  return arr.slice(arr.length - n);
 }
 
 /**
